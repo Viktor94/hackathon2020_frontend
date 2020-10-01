a0c48e069283a5447ed03c74d7e42bbef54b4018
alexfrenkel92 <alex.frenkel92@gmail.com>
2020-10-01 20:19:40 +0200

    cors imlemented

--- src/Services/user.service.js
+++ src/Services/user.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://cors-anywhere.herokuapp.com/https://hackathon-best-frontend.herokuapp.com";
+const API_URL = "https://cors-anywhere.herokuapp.com,https://hackathon-best-frontend.herokuapp.com";
 
 const login = (password, userName) => {
   return axios
